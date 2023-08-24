95689848fabcd61190ff84481d12021e264ea861
Elian Webster <elianwebster0@gmail.com>
2023-08-23 19:47:35 -0500

    + Mejora del código HTML & CSS

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 const text = "Mi Página Web";
 let index = 0;
-const speed = 100; // Velocidad de escritura en milisegundos
+const speed = 100; 
 let typingInterval;
-
 function typeEffect() {
     document.getElementById("typing-title").textContent = text.slice(0, index);
     index++;
-
     if (index > text.length) {
-        clearTimeout(typingInterval); // Detener el efecto al completar el texto
+        clearTimeout(typingInterval); 
     } else {
         typingInterval = setTimeout(typeEffect, speed);
     }
 }
-
-// Iniciar el efecto de typing cuando se cargue la página
 window.onload = function () {
     typeEffect();
 };
